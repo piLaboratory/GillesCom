8506637bbccd58abe0bc4078dfd10774041233c2
Andre Chalom <andrechalom@gmail.com>
2016-08-09 12:37:33 -0300

    Adding a stoch_environment function

---
+++ R/environment.R
@@ -0,0 +1,15 @@
+#' Random environment generator
+#'
+#' This function is a simple generator for a stochastic data frame
+#' that can be used to represent a randomly fluctuating environment
+#' for use in \code{\link{Init_Community}}.
+#'
+#' @export
+#' @rdname environment
+#' @param time Final time
+#' @param sd Standard deviation of the random walk process
+#' @param intervals Number of steps of the random walk
+stoch_environment <- function(time, sd, intervals = 10000) {
+    K <- exp(cumsum(rnorm(intervals, sd=sd)))
+    data.frame(time = seq(0, time, length=intervals), K)
+}
